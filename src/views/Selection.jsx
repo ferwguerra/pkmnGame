973f135035235ff4b2db2f852280b496c1968924
Fernando Guerra <fernando.guerra@adevinta.com>
2022-04-30 16:02:01 +0200

    Integrated with API to get first load of pokemons

--- src/views/Selection.jsx
+++ src/views/Selection.jsx
@@ -1,6 +1,49 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useReducer } from "react";
+import { types } from "../components/types/types";
 
 const Selection = ({ onReady }) => {
+
+  const initialStatePokemons = {
+    pokemons: []
+  }
+
+  const pokemonReducer = (state, action) => {
+    switch (action.type) {
+      case types.ADD_POKEMON:
+        return {
+          ...state,
+          pokemons: [...state.pokemons, action.payload]
+        }
+      default:
+        break;
+    }
+  }
+
+  const [pokemonsState, dispatch] = useReducer(pokemonReducer, initialStatePokemons);
+
+  useEffect(() => {
+    const loadInitialPokemons = async () => {
+      await getPokemons();
+    }
+    loadInitialPokemons();
+    return () => { }
+  }, []);
+
+  const getPokemons = () => {
+    axios("https://pokeapi.co/api/v2/pokemon?limit=150")
+      .then(response => {
+        response.data.results.forEach(pokemon => {
+          dispatch({
+            type: types.ADD_POKEMON,
+            payload: pokemon.name
+          });
+        });
+      })
+  }
+
+
+
   return (
     <>
       <div>
@@ -14,6 +57,13 @@ const Selection = ({ onReady }) => {
             onReady();
           }}
         />
+        <ul>
+          {pokemonsState.pokemons.map((pokemon, index) => (
+            <li key={index}>
+              {pokemon}
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
